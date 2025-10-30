2792723226e5cf423e8da085cc88d80a431ae547
PloyU <137448173+ArayaPloy@users.noreply.github.com>
2025-10-31 04:29:06 +0700

    vite server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // หรือใช้ '0.0.0.0' เพื่อเปิดให้เข้าถึงจาก local network
+    port: 5173, // port เดิม (ถ้าต้องการเปลี่ยนก็ได้)
+  }
 })
